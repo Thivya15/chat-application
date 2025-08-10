6f891c5cc3c2830683d6f13f15fa2e6ed6b4eb65
Thivya15 <thivyamahendran15@gmail.com>
2025-08-10 16:20:23 +0530

    controller folder and routes foder

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -2,19 +2,22 @@ const mongoose = require('mongoose')
 
 async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
 
         const connection = mongoose.connection
 
-        connection.on('connected',()=>{
-            console.log("Connect to DB")
+        connection.on('connected', () => {
+            console.log("Connected to DB")
         })
 
-        connection.on('error',(error)=>{
-            console.log("Something is wrong in mongodb ",error)
+        connection.on('error', (error) => {
+            console.log("MongoDB connection error: ", error)
         })
     } catch (error) {
-        console.log("Something is wrong ",error)
+        console.log("Something is wrong ", error)
     }
 }
 
